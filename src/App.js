84ac8d03adc2eb1dcc578904a7914f94654df9e1
Nadhir <mamokaze@gmail.com>
2020-12-13 03:12:08 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     useEffect(() => {
         setTimeout(() => {
             document.getElementsByClassName('App')[0].removeChild(document.getElementById('title-text'));
+            document.getElementById('footer').style.opacity = 1;
             newGame()
         }, 4000);
     }, [])
@@ -15,6 +16,9 @@ function App() {
                 <text x="50%" y="50%" fill="none" fontSize="8em" dominantBaseline="middle" textAnchor="middle">Sudoku !</text>
             </svg>
             <div id="container"></div>
+            <div id="footer" style={{ opacity: '0', transition:'opacity 2s' }}>
+                Created by <a href="https://github.com/nadhirxz" target="_blank">nadhirxz</a>
+            </div>
         </div>
     );
 }
